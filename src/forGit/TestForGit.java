66956826e604da7e0d3b5dc4b07df816e4de19ac
Admin <4evi4alova@gmail.com>
2016-10-19 21:47:39 +0300

    sxsdsa

---
+++ src/forGit/TestForGit.java
@@ -0,0 +1,11 @@
+package forGit;
+
+/**
+ * Created by Admin on 19.10.2016.
+ */
+public class TestForGit {
+
+    public static void main(String [] args){
+        System.out.println("HELLO !!!!!");
+    }
+}
